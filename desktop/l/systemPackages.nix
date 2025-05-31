77997000c4775e5a8d7439e5aa74b945253eb6a2
randomizedcoder dave.seddon.ca@gmail.com <dave.seddon.ca@gmail.com>
2025-05-31 16:04:28 -0700

    desktop

---
+++ desktop/l/systemPackages.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  # set at flake.nix level
+  nixpkgs.config.allowUnfree = true;
+
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Basic system tools
+    psmisc
+    vim
+    curl
+    wget
+    tcpdump
+    iproute2
+    nftables
+    iptables
+    pciutils
+    usbutils
+    iw
+    wirelesstools
+    wpa_supplicant
+    lldpd
+    #snmp seems to be needed by lldpd
+    net-snmp
+    neofetch
+    libxml2  # Added for bazel/clang development
+
+    # Wayland support
+    xwayland
+    meson
+    wayland-protocols
+    wayland-utils
+    wl-clipboard
+
+    # https://wiki.nixos.org/wiki/Flameshot
+    #(flameshot.override { enableWlrSupport = true; })
+  ];
+}
